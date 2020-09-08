6f6321d2703ef3f8ba0b60c926fea7a422514e1a
Maite Eguren Brown <maite.eguren.brown@gmail.com>
2020-09-08 16:03:03 +1000

    booking model created

--- app/models/jet.rb
+++ app/models/jet.rb
@@ -1,5 +1,6 @@
 class Jet < ApplicationRecord
   belongs_to :user
+  has_many :bookings
 
   validates :name, :description, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :capacity, :price, presence: true, numericality: { only_integer: true }
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_041650) do
+ActiveRecord::Schema.define(version: 2020_09_08_060220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "passengers"
+    t.integer "total_price"
+    t.string "booking_status"
+    t.string "destination"
+    t.bigint "jet_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["jet_id"], name: "index_bookings_on_jet_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "jets", force: :cascade do |t|
     t.string "name"
@@ -42,5 +57,7 @@ ActiveRecord::Schema.define(version: 2020_09_08_041650) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "jets"
+  add_foreign_key "bookings", "users"
   add_foreign_key "jets", "users"
 end
