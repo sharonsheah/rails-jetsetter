6d0201ae2b3304f7bd9cf5ce654369bfbc4249d7
Maite Eguren Brown <maite.eguren.brown@gmail.com>
2020-09-09 14:03:58 +1000

    where method added to jets index

--- app/controllers/jets_controller.rb
+++ app/controllers/jets_controller.rb
@@ -1,6 +1,14 @@
 class JetsController < ApplicationController
   def index
-    @jets = Jet.all
+    if params[:departure] && params[:passengers]
+      @jets = Jet.where("departure LIKE ?", "#{params[:departure]}")
+    elsif params[:departure]
+      @jets = Jet.where("departure LIKE ?", "#{params[:departure]}")
+    elsif params[:passengers]
+      @jets = Jet.where("passengers LIKE ?", "#{params[:passengers]}")
+    else
+      @jets = Jet.all
+    end
   end
 
   def show
