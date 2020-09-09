a309b98f292b075d86dee21614d121915753a733
Sharon Sheah <sharonsheah@gmail.com>
2020-09-09 12:28:30 +1000

    trying to seed. need to change description not to validate uniqueness

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,17 @@
 require 'faker'
 
+puts "Cleaning data..."
+
+User.destroy_all
+Jet.destroy_all
+
 puts 'Creating 1 default  user...'
 
 default_user = User.new(
     first_name: "Annmarie",
     last_name: "De Silva",
-    email: "user@user.com"
+    email: "user@user.com",
+    password: "password"
   )
 
 default_user.save!
@@ -16,7 +22,8 @@ puts 'Creating 10 fake users...'
   user = User.new(
     first_name: Faker::Name.unique.name,
     last_name: Faker::Name.last_name,
-    email: Faker::Internet.email
+    email: Faker::Internet.email,
+    password: "password"
   )
 
   user.save!
@@ -26,7 +33,7 @@ puts 'Creating 10 fake jets...'
 100.times do
   jet = Jet.new(
     name: Faker::Movies::StarWars.character,
-    description: Faker::Movies::StarWars.quoteFaker::Lorem.sentence,
+    description: Faker::Movies::StarWars.quote,
     pilot_name: Faker::Name.unique.name,
     capacity: rand(1..10),
     origin: Faker::Nation.capital_city,
