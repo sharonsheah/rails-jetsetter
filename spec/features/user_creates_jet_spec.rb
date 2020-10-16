24bccb4511df6b6996ccdd3cb2c575d0b507dbce
Maite Eguren Brown <maiteegurenbrown@Benjamins-MacBook-Air.local>
2020-10-16 18:53:31 +1100

    login function added for rspec, cannot make simple form fields work

--- spec/features/user_creates_jet_spec.rb
+++ spec/features/user_creates_jet_spec.rb
@@ -2,9 +2,11 @@ require "rails_helper"
 
 feature "User creates jet" do
     scenario "successfully" do
+        login_as nil
         visit root_path
         click_on 'List a new jet'
 
+    
         expect(page).to have_css 'form'
     end
 end
